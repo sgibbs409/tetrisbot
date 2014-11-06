3d7a118f6f66ac476462f79c4fa6096a85e4bfac
Mindy <mindyh@stanford.edu>
2014-11-06 14:37:22 -0800

    using tute4 as a base, writing own controller

--- tetrisbot.cpp
+++ tetrisbot.cpp
@@ -20,9 +20,9 @@ You should have received a copy of the GNU Lesser General Public
 License and a copy of the GNU General Public License along with
 scl. If not, see <http://www.gnu.org/licenses/>.
  */
-/* \file scl_tutorial5_multi_task.cpp
+/* \file scl_tutorial4_control_gc_op.cpp
  *
- *  Created on: Aug 10, 2014
+ *  Created on: Jul 30, 2014
  *
  *  Copyright (C) 2014
  *
@@ -40,9 +40,9 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 #include <scl/graphics/chai/ChaiGlutHandlers.hpp>
 #include <scl/control/task/CControllerMultiTask.hpp>
 #include <scl/control/task/tasks/data_structs/STaskOpPos.hpp>
+#include <scl/util/DatabaseUtils.hpp>
 //scl functions to simplify dynamic typing and data sharing
 #include <scl/robot/DbRegisterFunctions.hpp>
-#include <scl/util/DatabaseUtils.hpp>
 #include <scl/util/HelperFunctions.hpp>
 
 //For timing
@@ -53,6 +53,7 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 
 //Standard includes (for printing and multi-threading)
 #include <iostream>
+#include <fstream>
 #include <omp.h>
 
 //Freeglut windowing environment
@@ -60,60 +61,9 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 
 using namespace std;
 
-// constants of game board
-const int NUM_SQUARES_HIGH = 20;
-const int NUM_SQUARES_WIDE = 10;
-const float SQUARE_SIZE = 0.035;
-
-// Translation controls
-void moveLeft(Eigen::VectorXd & x_goal, int &curr_x, int &curr_y)
-{
-  if(curr_x > 0)
-    {
-      x_goal(1) -= SQUARE_SIZE;
-      curr_x--;
-      cout << "moveLeft" << endl;
-      cout << "currx: " << curr_x << "  curr_y: " << curr_y << endl;
-    }
-}
-
-void moveRight(Eigen::VectorXd & x_goal, int &curr_x, int &curr_y)
-{
-  if(curr_x < NUM_SQUARES_WIDE)
-    {
-      x_goal(1) += SQUARE_SIZE;
-      curr_x++;
-      cout << "moveRight" << endl;
-      cout << "currx: " << curr_x << "  curr_y: " << curr_y << endl;
-    }
-}
-
-void moveUp(Eigen::VectorXd & x_goal, int &curr_x, int &curr_y)
-{
-  if(curr_y < NUM_SQUARES_HIGH)
-    {
-      x_goal(2) += SQUARE_SIZE;
-      curr_y++;
-      cout << "moveUp" << endl;
-      cout << "currx: " << curr_x << "  curr_y: " << curr_y << endl;
-    }
-}
-
-void moveDown(Eigen::VectorXd & x_goal, int &curr_x, int &curr_y)
-{
-  if(curr_y > 0)
-    {
-      x_goal(2) -= SQUARE_SIZE;
-      curr_y--;
-      cout << "moveDown" << endl;
-      cout << "currx: " << curr_x << "  curr_y: " << curr_y << endl;
-    }
-}
-
-
 /** A sample application to demonstrate a physics simulation in scl.
  *
- * Moving forward from tutorial 4, we will now control the 6 DOF
+ * Moving forward from tutorial 3, we will now control the 6 DOF
  * demo robot (r6bot) with the physics engine running.
  *
  * SCL Modules used:
@@ -122,11 +72,11 @@ void moveDown(Eigen::VectorXd & x_goal, int &curr_x, int &curr_y)
  * 4. dynamics (control matrices)
  * 4. graphics (chai)
  * */
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  std::cout<<"\n***************************************\n";
-  std::cout<<"Standard Control Library Tutorial #5";
-  std::cout<<"\n***************************************\n";
+//  std::cout<<"\n***************************************\n";
+//  std::cout<<"Standard Control Library Tutorial #4";
+//  std::cout<<"\n***************************************\n";
 
   scl::SRobotParsed rds;     //Robot data structure....
   scl::SGraphicsParsed rgr;  //Robot graphics data structure...
@@ -136,16 +86,7 @@ int main(int argc, char** argv)
   scl::CDynamicsScl dyn_scl; //Robot kinematics and dynamics computation object...
   scl::CDynamicsTao dyn_tao; //Robot physics integrator
   scl::CParserScl p;         //This time, we'll parse the tree from a file...
-
-  scl::SControllerMultiTask rctr_ds; //A multi-task controller data structure
-  scl::CControllerMultiTask rctr;    //A multi-task controller
-  std::vector<scl::STaskBase*> rtasks;              //A set of executable tasks
-  std::vector<scl::SNonControlTaskBase*> rtasks_nc; //A set of non-control tasks
-  std::string must_use_robot;        //Used later for file error checks.
-  scl::STaskOpPos* rtask_hand;       //Will need to set hand desired positions etc.
-  scl::* rtask_hand;       //Will need to set hand desired positions etc.
-
-  sutil::CSystemClock::start(); //Start the clock
+  sutil::CSystemClock::start();
 
   /******************************Set up Shared Memory (Database)************************************/
   // This will help scl find the graphics files. Usually they are specified wrt. some
@@ -162,23 +103,9 @@ int main(int argc, char** argv)
   flag = flag && rgcm.init(rds);            //Simple way to set up dynamic tree...
   flag = flag && dyn_tao.init(rds);         //Set up integrator object
   flag = flag && dyn_scl.init(rds);         //Set up kinematics and dynamics object
-  flag = flag && rio.init(rds.name_,rds.dof_);        //Set up the I/O data structure
+  flag = flag && rio.init(rds.name_,rds.dof_);
   for(unsigned int i=0;i<rds.dof_;++i){ rio.sensors_.q_(i) = rds.rb_tree_.at(i)->joint_default_pos_; }
   if(false == flag){ return 1; }            //Error check.
-
-  /******************************Set up Controller Specification************************************/
-  // Read xml file info into task specifications.
-  flag = p.readTaskControllerFromFile(fname,"opc",must_use_robot,rtasks,rtasks_nc);
-  if(must_use_robot != "PumaBot") {flag = false;} //Error check for file consistency
-  flag = flag && rctr_ds.init("opc",&rds,&rio,&rgcm); //Set up the control data structure..
-  // Tasks are initialized after find their type with dynamic typing.
-  flag = flag && scl_registry::registerNativeDynamicTypes();
-  flag = flag && scl_util::initMultiTaskCtrlDsFromParsedTasks(rtasks,rtasks_nc,rctr_ds);
-  flag = flag && rctr.init(&rctr_ds,&dyn_scl);        //Set up the controller (needs parsed data and a dyn object)
-  if(false == flag){ return 1; }            //Error check.
-
-  rtask_hand = dynamic_cast<scl::STaskOpPos*>( *(rctr_ds.tasks_.at("hand")) );
-  if(NULL == rtask_hand)  {return 1;}       //Error check
 
   /******************************ChaiGlut Graphics************************************/
   glutInit(&argc, argv); // We will use glut for the window pane (not the graphics).
@@ -191,76 +118,74 @@ int main(int argc, char** argv)
 
   /******************************Simulation************************************/
   // Now let us integrate the model for a variety of timesteps and see energy stability
-  std::cout<<"\nIntegrating the robot's physics. \nWill test two different controllers.\n Press (x) to exit at anytime.";
-  long long iter = 0; double dt=0.0001;
+  //std::cout<<"\nIntegrating the r6bot's physics. \nWill test two different controllers.\n Press (x) to exit at anytime.";
+  long iter = 0, n_iters=200000; double dt=0.0001;
 
   omp_set_num_threads(2);
   int thread_id; double tstart, tcurr; flag = false;
+  Eigen::VectorXd hpos(6); //control position of op-point wrt. hand
+  hpos.setZero();
+  Eigen::MatrixXd J;
+  Eigen::VectorXd x(6);
+  Eigen::VectorXd x_des(6);
+  Eigen::VectorXd x_init(6);
+  Eigen::VectorXd dx(6);
+  scl::SRigidBodyDyn *ee = rgcm.rbdyn_tree_.at("end-effector");
 
 #pragma omp parallel private(thread_id)
   {
     thread_id = omp_get_thread_num();
-    bool is_button_pressed = false; // for debouncing
     if(thread_id==1) //Simulate physics and update the rio data structure..
     {
       tstart = sutil::CSystemClock::getSysTime(); iter = 0;
-
-      // initialize
-      rtask_hand->x_goal_(0) = 0.2;
-      rtask_hand->x_goal_(1) = 0;
-      rtask_hand->x_goal_(2) = 0;
-      int curr_x = NUM_SQUARES_WIDE/2; int curr_y = NUM_SQUARES_WIDE/2;
-
       while(true == scl_chai_glut_interface::CChaiGlobals::getData()->chai_glut_running)
       {
         tcurr = sutil::CSystemClock::getSysTime();
 
-        // translate on awsd key press
-        if(scl_chai_glut_interface::CChaiGlobals::getData()->keys_active['a'])
-         {
-            if(!is_button_pressed)
-              moveLeft(rtask_hand->x_goal_, curr_x, curr_y);
-            is_button_pressed = true; // for debouncing
-         }
-        else if(scl_chai_glut_interface::CChaiGlobals::getData()->keys_active['s'])
-        {
-            if(!is_button_pressed)
-		moveDown(rtask_hand->x_goal_, curr_x, curr_y);
-	    is_button_pressed = true; // for debouncing
-	} else if(scl_chai_glut_interface::CChaiGlobals::getData()->keys_active['d'])
-	{
-	    if(!is_button_pressed)
-		moveRight(rtask_hand->x_goal_, curr_x, curr_y);
-	    is_button_pressed = true; // for debouncing
-	} else if(scl_chai_glut_interface::CChaiGlobals::getData()->keys_active['w'])
-	{
-	    if(!is_button_pressed)
-		moveUp(rtask_hand->x_goal_, curr_x, curr_y);
-	    is_button_pressed = true; // for debouncing
-	} else {
-	    is_button_pressed = false;
-	}
+        // Compute kinematic quantities
+        dyn_scl.computeTransformsForAllLinks(rgcm.rbdyn_tree_,rio.sensors_.q_);
+        dyn_scl.computeJacobianWithTransforms(J,*ee,rio.sensors_.q_,hpos.segment(0,3));
 
-        // Compute control forces (note that these directly have access to the io data ds).
-        rctr.computeDynamics();
-        rctr.computeControlForces();
+        // update current end-effector position and orientation
+        if(false == flag) {
+            Eigen::Vector3d temp =  hpos.segment(0,3);
+	     x_init.segment(0,3) = ee->T_o_lnk_ * temp;
+	     x_init.segment(2,3) = ee->T_o_lnk_.rotation() * hpos.segment(2,3);
+	     flag = true;
+        }
+        Eigen::Vector3d temp =  hpos.segment(0,3);
+        x.segment(0,3)  = ee->T_o_lnk_ * temp;
+        x.segment(2,3)  = ee->T_o_lnk_.rotation() * hpos.segment(2,3);
+
+        dx = J * rio.sensors_.dq_;
+
+	float kp = 1;
+	float kv = 1;
+
+	x_des.setZero();
+        x_des(0) = x_init(0)+0.1;
+        x_des(1) = x_init(1)+0.1;
+        rio.actuators_.force_gc_commanded_ = J.transpose() * (kp*(x_des-x) - kv * dx) + rgcm.force_gc_grav_;
+
+        cout << "x " << x << endl;
+        cout << "dx " << dx << endl;
+        cout << "x_des " << x_des << endl;
 
         // Integrate the dynamics
-        dyn_tao.integrate(rio,dt); iter++;
+        dyn_tao.integrate(rio,dt); iter++; const timespec ts = {0, 5000};/*.05ms*/ nanosleep(&ts,NULL);
+
       }
       //Then terminate
       scl_chai_glut_interface::CChaiGlobals::getData()->chai_glut_running = false;
     }
-    else  //Read the rio data structure and updated rendered robot..
+    else  //Read the rio data structure and updated rendererd robot..
       while(true == scl_chai_glut_interface::CChaiGlobals::getData()->chai_glut_running)
       { glutMainLoopEvent(); const timespec ts = {0, 15000000};/*15ms*/ nanosleep(&ts,NULL); }
   }
 
   /******************************Exit Gracefully************************************/
-  std::cout<<"\n\n\tSystem time = "<<sutil::CSystemClock::getSysTime()-tstart;
-  std::cout<<"\n\tSimulated time = "<<static_cast<double>(iter)*dt;
-  std::cout<<"\n\nExecuted Successfully";
-  std::cout<<"\n**********************************\n"<<std::flush;
+//  std::cout<<"\n\nExecuted Successfully";
+//  std::cout<<"\n**********************************\n"<<std::flush;
 
   return 0;
 }
