c571f14aebcf4fc60607ebe09d5eaf5d09d625d6
Mindy Huang <mindyh@stanford.edu>
2014-11-20 11:12:27 -0800

    added basc cv

--- simple_client_2014/PumaClient.cpp
+++ simple_client_2014/PumaClient.cpp
@@ -21,12 +21,18 @@
 #include <stdlib.h>
 #include "math.h"
 
+// opencv libraries
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 using namespace std;
+using namespace cv;
 
 // constants of game board
 const int NUM_SQUARES_HIGH = 20;
 const int NUM_SQUARES_WIDE = 10;
-const float SQUARE_SIZE = 0.035;
+const float SQUARE_SIZE = 0.005;
 
 // number of arguments describing the end-effector - x,y,z,alpha,beta,gamma.
 const int NUM_ARGS = 6;
@@ -52,11 +58,151 @@ void moveTo(float *x_goal, int target_x, int target_y, int &curr_x, int &curr_y,
   cout << "currx: " << curr_x << "  curr_y: " << curr_y << " curr_rotation:  " << x_goal[GAMMA] << endl;
 }
 
+void waitForStart()
+{
+    cout << "Float..." << endl;
+	cout << "Disengage E-stop and hit 's'" << endl;
+	char key;
+	while(1)
+	{
+		 cin >> key;
+		 if(key == 's')
+		 break;
+	}
+	cout << "Disengaged..." << endl;
+	cout << "Starting tasks..." << endl;
+}
+
+
+// Thresholding constants
+int iLowH = 166;
+int iHighH = 178;
+
+int iLowS = 201; 
+int iHighS = 255;
+
+int iLowV = 114;
+int iHighV = 255;
+
+int MAX_X;
+int MAX_Y;
+
+VideoCapture initCamera()
+{
+	VideoCapture cap(0); //capture the video from web cam
+
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return NULL;
+    }
+
+	cout << "initing" << endl;
+
+	Mat tempMat;
+	cap.read(tempMat);
+	MAX_X = tempMat.cols;
+	MAX_Y = tempMat.rows; 
+
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 360); //Hue (0 - 360)
+	createTrackbar("HighH", "Control", &iHighH, 360);
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+	return cap;
+}
+
+Point GetCentroid(Mat &img)
+{
+	Point retval;
+
+	//Calculate the moments of the thresholded image
+	Moments oMoments = moments(img);
+	double dM01 = oMoments.m01;
+	double dM10 = oMoments.m10;
+	double dArea = oMoments.m00;
+
+	// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+	if (dArea > 10000)
+	{
+		//calculate the position
+		retval.x = dM10 / dArea;
+		retval.y = dM01 / dArea;        
+        
+		//Draw a circle on the center point
+		circle(img, retval, 3, Scalar(255,255,255), 2);
+
+		// draw target, center of the image
+		circle(img, Point(MAX_X/2, MAX_Y/2), 3, Scalar(100,100,255),2);
+	}
+
+	return retval;
+}
+
+bool UpdateCamera(VideoCapture &cap, Mat &img)
+{
+    Mat imgOriginal;
+
+    bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+    if (!bSuccess) //if not success, break loop
+    {
+        cout << "Cannot read a frame from video stream" << endl;
+        return false;
+    }
+
+	Mat imgHSV;
+	cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+		
+	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), img); //Threshold the image
+      
+	//morphological opening (remove small objects from the foreground)
+	erode(img, img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+	dilate(img, img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+	//morphological closing (fill small holes in the foreground)
+	dilate( img, img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+	erode(img, img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+	GetCentroid(img);
+
+	imshow("Thresholded Image", img); //show the thresholded image
+	imshow("Original", imgOriginal); //show the original image
+
+	if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+	{
+		cout << "esc key is pressed by user" << endl;
+	}
+
+	return true;
+}
+
+// Using a webcam on the end-effector.
+void AimEndEffector(RobotCom* PumaRobot)
+{
+	//Point targetPoint = 
+}
+
+void pickUpBlock()
+{
+	// fill in
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//RobotCom* PumaRobot = new RobotCom();
+    //PumaRobot->_float();
+	//waitForStart();
 
-	//PumaRobot->_float();
+	// Initialize Camera
+	VideoCapture cap = initCamera();
 	
 	// initialize game board variables
 	int curr_x = NUM_SQUARES_WIDE/2; int curr_y = NUM_SQUARES_WIDE/2;
@@ -67,16 +213,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	x_goal[0] =  0.4; // set the end-effector forwad a bit in the x-axis direction
 
 	// Threading stuffs
-	omp_set_num_threads(2);
-	int thread_id;
-	#pragma omp parallel private(thread_id)
+	#pragma omp parallel num_threads(2)
 	  {
-		thread_id = omp_get_thread_num();
+		int thread_id = omp_get_thread_num();
 		
-		if(thread_id==1) 
+		if(thread_id==0) 
 		{
+			Mat img;
 			while(true) {
-				//PumaRobot->control(GOTO, x_goal, NUM_ARGS);
+				//if(!UpdateCamera(cap, img)) break;
 			}
 		}
 		else {
